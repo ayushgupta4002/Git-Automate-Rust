ed8f4f28ae209dfea77c701ff310c7a0dbeafa66
Ayush <ayush4002gupta@gmail.com>
2024-03-07 22:09:15 +0530

    refcatored some code

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 
 fn automate(){
 
-    let commit_message = commit_msg();
+    // let commit_message = commit_msg();
 
     let add_command = Command::new("git")
     .arg("add")
@@ -19,7 +19,7 @@ fn automate(){
     let commit_command =  Command::new("git")
     .arg("commit")
     .arg("-m")
-    .arg(commit_message.clone()).output().expect("commit command could not be executed");
+    .arg(commit_msg()).output().expect("commit command could not be executed");
 
     if !commit_command.status.success(){
         eprintln!("Commit command failed !");
@@ -29,7 +29,7 @@ fn automate(){
     let push_command = Command::new("git")
     .arg("push")
     .arg("origin")
-    .arg("master")
+    .arg(branch_name())
     .output().expect("push command could not be executed");
 
     if !push_command.status.success(){
@@ -37,7 +37,7 @@ fn automate(){
         exit(1);
     }
 
-    println!("Successfully pushed all code with Commit message : {} " ,&commit_message);
+    // println!("Successfully pushed all code with Commit message : {} ");
 
 }
 
@@ -48,14 +48,14 @@ fn commit_msg() -> String {
     commit_message
 }
 
-// fn branch_name() -> String {
-//     let mut branch = String::new();
-//     println!("please enter your branch name:");
-//     io::stdin().read_line(&mut branch).expect("sorry operation could not be performed");
-//     branch
-// }
+fn branch_name() -> String {
+    let mut branch = String::new();
+    println!("please enter your branch name:");
+    io::stdin().read_line(&mut branch).expect("sorry operation could not be performed");
+    branch
+}
 
 fn main() {
-    println!("W E L C O M E!");
+    println!("Hello, world!");
     automate();
 }
