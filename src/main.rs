960b0d3fc98f97795d736137a64e431d8819318b
Ayush <ayush4002gupta@gmail.com>
2024-03-07 21:57:18 +0530

    status::project completed and ready to be pushed

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use std::process::{Command,exit};
+use std::io;
+
+fn automate(){
+
+    let add_command = Command::new("git")
+    .arg("add")
+    .arg("-A")
+    .output()
+    .expect("add command could not be executed");
+
+    if !add_command.status.success(){
+        eprintln!("add command failed !");
+        exit(1);
+    }
+
+    let commit_command =  Command::new("git")
+    .arg("commit")
+    .arg("-m")
+    .arg(commit_msg()).output().expect("commit command could not be executed");
+
+    if !commit_command.status.success(){
+        eprintln!("Commit command failed !");
+        exit(1);
+    }
+
+    let push_command = Command::new("git")
+    .arg("push")
+    .arg("origin")
+    .arg("master")
+    .output().expect("push command could not be executed");
+
+    if !push_command.status.success(){
+        eprintln!("push command could not be executed");
+        exit(1);
+    }
+
+}
+
+fn commit_msg() -> String {
+    let mut guess = String::new();
+    println!("please enter your commit message: !");
+    io::stdin().read_line(&mut guess).expect("sorry operation could not be performed");
+    guess
+}
+
+fn main() {
+    println!("Hello, world!");
+    automate();
+}
